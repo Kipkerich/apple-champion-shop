1fe94f893499ceaf8109a1a9b63df56420a5a0d5
Kipkerich <jkikerich@kabarak.ac.ke>
2023-11-14 01:25:33 +0300

    Add product functionality

--- products/views.py
+++ products/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import ProductForm
+from django.contrib import messages
 
 
 # Create your views here.
@@ -7,7 +9,18 @@ def products(request):
 
 
 def add_products(request):
-    return render(request, 'products/add-products.html')
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Product saved successfully')
+            return redirect('add-products-url')
+        else:
+            messages.error(request, 'Product saving failed')
+            return redirect('add-products-url')
+    else:
+        form = ProductForm()
+    return render(request, 'products/add-products.html', {'form': form})
 
 
 def update_products(request):

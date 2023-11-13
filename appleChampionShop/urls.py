e1428fb49b5d76ad7070002da1a43e3ea7760f56
Kipkerich <jkikerich@kabarak.ac.ke>
2023-11-13 07:53:21 +0300

    Started working on product app

--- appleChampionShop/urls.py
+++ appleChampionShop/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from . import views as my_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',my_views.home, name='home-url'),
-    path('shop/',my_views.shop, name='shop-url'),
-    path('blog/',my_views.blog, name='blog-url'),
+    path('', my_views.home, name='home-url'),
+    path('shop/', my_views.shop, name='shop-url'),
+    path('blog/', my_views.blog, name='blog-url'),
+    path('auth/', include('auth.urls')),
+    path('products/', include('products.urls'))
     ]
